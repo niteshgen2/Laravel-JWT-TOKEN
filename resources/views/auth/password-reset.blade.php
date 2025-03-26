fc76e0a381e82c74f0ab786d9931376186e32fb7
niteshgen2 <nitesh@gen2.in>
2025-03-26 16:07:31 +0530

    first commit

---
+++ resources/views/auth/password-reset.blade.php
@@ -0,0 +1,3 @@
+<h2>Password Reset</h2>
+
+<a href="{{ $url }}">Password Reset</a>
--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,25 @@
 <?php
 
+use App\Http\Controllers\ForgotPasswordController;
+use App\Http\Controllers\ResetPasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 
-
-
 Route::group(['prefix' => 'auth'], function ($router) {
-    Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
     
 });
+
+Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail']);
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.reset');
 
 
 Route::middleware(['auth:api'])->group(function(){
